f259daef1f5cc79dce304de505acafef2845d672
kylebaugh <kylebaugh1@gmail.com>
2021-10-26 11:40:21 -0600

    added rollbar access token

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path')
 const Rollbar = require('rollbar')
 
 let rollbar = new Rollbar({
-    accessToken: '',
+    accessToken: 'bb5265b4d4284924bda2a7f89fa89f11',
     captureUncaught: true,
     captureUnhandledRejections: true
 })
@@ -14,6 +14,7 @@ const port = process.env.PORT || 4545
 
 app.get('/', (req, res)=> {
     res.sendFile(path.join(__dirname, '/public/index.html'))
+    rollbar.info('html file served successfully')
 })
 
 app.listen(port, () => console.log(`Take us to warp ${port}`))
